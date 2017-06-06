d05c68d0dde5c53d5b967390d3b96b54ea1ce0c6
Alisher778 <alisher.musurmonov89@gmail.com>
2017-06-05 21:57:31 -0400

    added react-bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-
+import App from './components/App.js';
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
